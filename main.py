03d3b71ca3bd4e5b485f108346494bb44d5642ac
AJ <149464895+Atishay9828@users.noreply.github.com>
2025-06-19 17:02:33 +0530

    add desc,link , image

--- main.py
+++ main.py
@@ -15,10 +15,18 @@ with col2 :
                 applications in different desktop or web based graphical user interface."""
     st.info(content)
 st.write("Below you can find some of the apps i built in Python. Feel free to contact me !")
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.1,1.5])
 with col3 :
     for index , row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 with col4 :
     for index , row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
