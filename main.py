7135855b868a631d2a3e5c495b8da9e2538cd14d
AJ <149464895+Atishay9828@users.noreply.github.com>
2025-06-19 16:35:47 +0530

    render titles

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("data.csv" , sep = ";")
 
 st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
@@ -8,6 +11,14 @@ with col2 :
     st.title("Atishay Jain")
     content = """Hi my name is AJ and this is my portfolio website for the 
                 60 days 20 apps python project from udemy here i learn to use 
-                different python packages that are usually used to code amazing
+                different python pack ages that are usually used to code amazing
                 applications in different desktop or web based graphical user interface."""
     st.info(content)
+st.write("Below you can find some of the apps i built in Python. Feel free to contact me !")
+col3,col4 = st.columns(2)
+with col3 :
+    for index , row in df[:10].iterrows():
+        st.header(row["title"])
+with col4 :
+    for index , row in df[10:].iterrows():
+        st.header(row["title"])
